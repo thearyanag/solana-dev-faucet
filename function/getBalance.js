86d6f4964519d67651632a49aa1a94a1b54c717f
aryan <agarwalaryan139@gmail.com>
2023-07-20 16:54:29 +0530

    fix: rpc

--- function/getBalance.js
+++ function/getBalance.js
@@ -1,6 +1,6 @@
 const {  Connection , PublicKey } = require("@solana/web3.js");
 
-const connection = new Connection("https://api.devnet.solana.com");
+const connection = new Connection("https://devnet.helius-rpc.com/?api-key=9dd22d4f-02c8-4c19-9f61-1399587b0626");
 
 const getBalance = async (publicKey) => {
   const balance = await connection.getBalance(publicKey);
